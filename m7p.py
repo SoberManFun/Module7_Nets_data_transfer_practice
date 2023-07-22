4f48c705661987b45fdc37c0e251ad7ae3c8b7cb
SoberManFun <treg3359@gmail.com>
2023-07-22 23:49:22 +0300

    Вторая попытка сдать практику из Модуля 7

--- m7p.py
+++ m7p.py
@@ -1,21 +1,21 @@
 #___________________МОДУЛЬ_7__УРОВЕНЬ1_НАЧАЛО____________________
-# from bs4 import BeautifulSoup
-# import requests
+from bs4 import BeautifulSoup
+import requests
 
-# header = {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-#     'Accept-Language': 'en-US,en;q=0.9',
-# }
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
 
-# page = requests.get('https://mfd.ru/currency/?currency=USD', headers=header)
+page = requests.get('https://mfd.ru/currency/?currency=USD', headers=header)
 
-# soup = BeautifulSoup(page.text, 'html.parser')
+soup = BeautifulSoup(page.text, 'html.parser')
 
-# exchangeTable = soup.find('table', {'class': 'mfd-table mfd-currency-table'})
+exchangeTable = soup.find('table', {'class': 'mfd-table mfd-currency-table'})
 
-# exchanges = exchangeTable.find_all('td')
+exchanges = exchangeTable.find_all('td')
 
-# print(*[exchange.text for exchange in exchanges])
+print(*[exchange.text for exchange in exchanges])
 
 # # # ____________Удобочитаемый_вывод_данных_о_курсе_доллара________________
 # ex1 = [exchange.text for exchange in exchanges]
@@ -23,14 +23,34 @@
 # print(*(ex1[i:i + 3] for i in range(0, len(ex1), 3)), sep = "\n")
 #___________________МОДУЛЬ_7__УРОВЕНЬ1_КОНЕЦ____________________
 
-#___________________МОДУЛЬ_7__УРОВЕНЬ2_НАЧАЛО(доп модуль PyQt5)____________________
-# import matplotlib
-# import matplotlib.pyplot as plt
+#___________________МОДУЛЬ_7__УРОВЕНЬ2_НАЧАЛО___________________
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tckr
 
-# matplotlib.use('Qt5Agg')
+graphData = [exchange.text for exchange in exchanges]
 
-# print(matplotlib.get_backend())
+graphData.reverse()
 
-# plt.plot([2, 1, -4, 7, 12, 13, 25])
-# plt.show()
+dates =  [item.replace('с ', '') for item in graphData[2::3]]
+values = [float(item) for item in graphData[1::3]]
+changes = graphData[::3]
+
+fig, ax = plt.subplots()
+ax.yaxis.set_major_formatter(tckr.FormatStrFormatter('%.4f'))
+ax.plot(dates, values)
+ax.set_yticks(values)
+ax.set_xlabel('Дата')
+ax.set_ylabel('Курс USD/RUB')
+ax.set_title(f'График изменения курса доллара c {dates[0]} по {dates[len(dates)-1]}')
+plt.xticks(rotation=60)
+
+# Отображение изменений курса на графике
+def annotate_changes(x, y, changes):
+    for i in range(len(x)):
+        change = changes[i]
+        ax.annotate(f'{change}', (x[i], y[i]), textcoords="offset points", xytext=(0,10), ha='center')
+
+annotate_changes(dates, values, changes)
+
+plt.show()
 #___________________МОДУЛЬ_7__УРОВЕНЬ2_КОНЕЦ____________________
